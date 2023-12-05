8c3bc5213a0ebd10a4e4cd5ac99d5d5f59154080
Arunachalam13 <karunachalam406@gmail.com>
2023-12-05 17:05:06 +0530

    Added the GettingStarted sample.

---
+++ GettingStarted/GettingStarted/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace GettingStarted
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            List<string> list = new List<string>();
+            list.Add("Home");
+            list.Add("Profile");
+            list.Add("Inbox");
+            list.Add("Out box");
+            list.Add("Sent");
+            list.Add("Draft");
+            listView.ItemsSource = list;
+        }
+
+        private void hamburgerButton_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            // Your codes here
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+
+}
